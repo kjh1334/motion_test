36b454782bae91f60ece5feb9ae5155bcd230b8d
kjh1334 <78242136+kjh1334@users.noreply.github.com>
2022-01-13 12:06:07 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,38 +1,42 @@
-input.onButtonPressed(Button.A, function () {
-    run = 1
-    count = 0
-})
-input.onButtonPressed(Button.B, function () {
-    run = 0
-    count = 0
-    pins.servoWritePin(AnalogPin.P1, 0)
-})
-let count = 0
-let run = 0
 led.enable(false)
-run = 0
-count = 0
+let run = 1
+pins.digitalWritePin(DigitalPin.P5, 1)
+pins.digitalWritePin(DigitalPin.P6, 0)
+let count = 0
 pins.servoWritePin(AnalogPin.P1, 0)
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P9, PinPullMode.PullUp)
 basic.forever(function () {
     if (run == 1) {
+        if (pins.digitalReadPin(DigitalPin.P9) == 0) {
+            run = 0
+            pins.digitalWritePin(DigitalPin.P5, 0)
+            pins.digitalWritePin(DigitalPin.P6, 1)
+        }
         if (count < 60) {
+            pins.servoWritePin(AnalogPin.P1, 12)
             pins.digitalWritePin(DigitalPin.P2, 1)
-            pins.servoWritePin(AnalogPin.P1, 10)
-            basic.pause(250)
-            pins.servoWritePin(AnalogPin.P1, 20)
-            basic.pause(250)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            pins.servoWritePin(AnalogPin.P1, 10)
-            basic.pause(250)
+            basic.pause(500)
             pins.servoWritePin(AnalogPin.P1, 0)
-            basic.pause(300)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(500)
             count += 1
         } else if (count < 180) {
-            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P3, 1)
+            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P3, 0)
+            basic.pause(500)
             count += 1
         } else {
             count = 0
             pins.servoWritePin(AnalogPin.P1, 0)
+        }
+    } else {
+        if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+            run = 1
+            count = 0
+            pins.digitalWritePin(DigitalPin.P5, 1)
+            pins.digitalWritePin(DigitalPin.P6, 0)
         }
     }
 })
